8ca9024150b430df3accd8461bc360cee690beba
luca <luc.due@tim.it>
2025-10-05 23:22:40 +0200

    Jot down the logic and base template, add refactor + tests

--- producer/producer.ts
+++ producer/producer.ts
@@ -20,13 +20,55 @@ const main = async () => {
 
   rl.on('line', async (data: any) => {
     console.log("output => ", data)
-    await producer.send({
-      topic: 'all',
-      messages: [
-        {value: data}
-      ]
-    })
+    if(data.length){
+      await dispatchToTopics(data);
+    }
   })
 }
 
+async function dispatchToTopics(data: string): Promise<void>{
+  let topicsToSend = ['all'];
+  await producer.send({
+    topic: 'all',
+    messages: [
+      {value: data}
+    ]
+  });
+
+  if(isNum(data[0])) {
+    console.log( '### juwst to all >  ',  ) // MA SIIII che basta che va
+  } else if (startsInVowel(data[0])) {
+    topicsToSend.push('vowel');
+    console.log( '### sentTo vowel >  ',  ) // MA SIIII che basta che va
+  } else {
+    topicsToSend.push('consonant');
+    console.log( '### sentTo CONSONANT >  ',  ) // MA SIIII che basta che va
+  }
+  
+  topicsToSend.forEach(async (to) => {
+    await producer.send({
+      topic: to,
+      messages: [
+        {value: data}
+      ]
+    });
+  })
+  // console.log( '### sent to kafka >  ' ) // MA SIIII che basta che va
+}
+
+function startsInVowel(char: string){
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  return vowels.includes(char.toLowerCase());
+}
+
+function isNum(char:string){
+  let num = parseInt(char);
+
+  return num != undefined || !isNaN(num);
+}
+
+// function test(){
+//   let word = 'culo';
+// }
 main();
+// test();
